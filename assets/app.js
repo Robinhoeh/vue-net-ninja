73b0ad72b89899577fa41d62c3297efc13e422c2
Robin Watson <drumsrobin@gmail.com>
2018-10-21 09:50:26 -0400

    added event listener that updates data

--- assets/app.js
+++ assets/app.js
@@ -4,11 +4,24 @@ new Vue({
     name: 'Robin',
     job: 'Dev',
     website: 'https://www.google.com',
-    websiteTag: '<a href="https://www.google.com">Google</a>'
+    websiteTag: '<a href="https://www.google.com">Google</a>',
+    age: 25,
+    x: 0,
+    y: 0
   },
   methods: {
     greet(timeOfDay) {
       return `Good ${timeOfDay}, ${this.name}`
+    },
+    addAge(inc) {
+      this.age += inc
+    },
+    subtractAge(dec) {
+      this.age -= dec
+    },
+    updatePostion(e) {
+      this.x = e.offsetX
+      this.y = e.offsetY
     }
   }
 });
